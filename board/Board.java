e5aff740b32c09b236e0b48091d001e34dc5a270
skytree33157 <handaewun@gmail.com>
2026-09-20 17:18:34 +0900

    add getWidth and Height

--- board/Board.java
+++ board/Board.java
@@ -1,24 +1,46 @@
 package board;
 
 public class Board {
-    private static final int ROW=20;
-    private static final int COL=10;
+    private static final int ROW = 20;
+    private static final int COL = 10;
 
     private int[][] board;
 
-    // 게임보드 constructor
     public Board() {
         board = new int[ROW][COL];
     }
 
-    // 게임보드 반환
     public int[][] getBoard() {
         return board;
     }
 
-    // 최하단으로 내려온 block 위치를 보드에 저장
     public void setBlock(int row, int col, int value) {
-        if(row>=0 && row<ROW && col>=0 && col<COL)
+        if (row >= 0 && row < ROW && col >= 0 && col < COL) {
             this.board[row][col] = value;
+        }
+    }
+
+    public void addBlock(int x, int y, int[][] shape) {
+        if (shape == null) {
+            return;
+        }
+
+        for (int row = 0; row < shape.length; row++) {
+            for (int col = 0; col < shape[row].length; col++) {
+                if (shape[row][col] != 0) {
+                    int boardRow = y + row;
+                    int boardCol = x + col;
+                    setBlock(boardRow, boardCol, shape[row][col]);
+                }
+            }
+        }
+    }
+
+    public int getWidth() {
+        return COL;
+    }
+
+    public int getHeight() {
+        return ROW;
     }
 }
