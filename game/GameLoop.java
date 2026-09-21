2b524e4446eb7f86931d997d1c4124f83ce5c452
skytree33157 <handaewun@gmail.com>
2026-09-21 20:36:35 +0900

    add move and rotate with keyboard (FR-09)

--- game/GameLoop.java
+++ game/GameLoop.java
@@ -48,8 +48,8 @@ public class GameLoop implements Runnable {
         int nextY = y + 1;
         
         // 다음 위치가 유효한지 확인
-        if (isValidPosition(x, nextY)) {
-            block.setY(nextY);
+        if (board.isValidPosition(block.getShape(),x, nextY)) {
+            block.moveDown();
         } else { // 블록이 더 이상 내려갈 수 없으면 현재 위치에 블록을 고정 후 새로운 블록 생성
             board.addBlock(x, y, block.getShape());
 
@@ -65,34 +65,32 @@ public class GameLoop implements Runnable {
         }
     }
 
-    // 블록이 특정 위치에 놓일 수 있는지 확인하는 메서드
-    private boolean isValidPosition(int targetX, int targetY) {
-        int[][] shape = block.getShape();
-        
-        // 블록의 각 칸을 검사하여 충돌 여부 확인
-        for (int row = 0; row < shape.length; row++) {
-            for (int col = 0; col < shape[row].length; col++) {
-                // 블록이 실제로 존재하는 칸만 검사
-                if (shape[row][col] != 0) {
-                    int boardX = targetX + col;
-                    int boardY = targetY + row;
-
-                    // 블록이 보드 내부에 있는지 검사
-                    if (boardX < 0 || boardX >= board.getWidth() || boardY >= board.getHeight()) {
-                        return false;
-                    }
-
-                    // 다른 고정된 블록과 충돌하는지 검사 
-                    if (boardY >= 0) {
-                        int[][] curBoard = board.getBoard();
-                        if (curBoard[boardY][boardX] != 0) {
-                            return false;
-                        }
-                    }
-                }
-            }
+    public void moveLeftAction() {
+        int targetX = block.getX() - 1;
+        if (board.isValidPosition(block.getShape(), targetX, block.getY())) {
+            block.moveLeft();
         }
-        return true;
+    }
+
+    public void moveRightAction() {
+        int targetX = block.getX() + 1;
+        if (board.isValidPosition(block.getShape(), targetX, block.getY())) {
+            block.moveRight();
+        }
+    }
+
+    public void moveDownAction() {
+        int targetY = block.getY() + 1;
+        if (board.isValidPosition(block.getShape(), block.getX(), targetY)) {
+            block.moveDown();
+        }
+    }
+
+    public void rotateAction(){
+        int[][] rotatedShape = block.getRotate();
+        if (board.isValidPosition(rotatedShape, block.getX(), block.getY())) {
+            block.rotate();
+        }
     }
 
     // 하강 속도 증가시키는 메서드
