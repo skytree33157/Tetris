b995c459a5deda34bb798f79ffa20ae909121f37
skytree33157 <handaewun@gmail.com>
2026-09-25 21:19:45 +0900

    add pause (FR-12)

--- game/GameController.java
+++ game/GameController.java
@@ -12,6 +12,16 @@ public class GameController extends KeyAdapter {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        // P 키 입력 시 isPaused 전환 후 종료
+        if(e.getKeyCode() == KeyEvent.VK_P) {
+            gameLoop.togglePause();
+            return;
+        }
+        // 일시정지이면 키 입력 무시
+        if(gameLoop.isPaused()) {
+            return;
+        }
+
         switch (e.getKeyCode()) {
             case KeyEvent.VK_LEFT:
                 gameLoop.moveLeftAction();
